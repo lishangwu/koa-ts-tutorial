ebe85ca74a8b5d1d5581164eb27b2484e2a68968
lsw@autobotstech.com <lsw@autobotstech.com>
2020-06-14 18:59:48 +0800

    test

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ https://www.youtube.com/watch?v=bSVfmDbAsQg&amp;list=PL1sZ2OJpPmWZL0iwsy5ekCMHQ-
 npm init --force
 
 npm i -D @types/redis @types/jest @types/koa @types/koa-bodyparser @types/koa-logger @types/koa-router @types/koa2-cors @types/node @types/supertest husky jest prettier pretty-quick superjest ts-jest ts-node-dev
+
+"@types/koa": "^2.11.3",
+    "@types/koa-bodyparser": "^4.3.0",
+    "@types/koa-logger": "^3.1.1",
+    "@types/koa-router": "^7.4.1",
+    "@types/koa2-cors": "^2.0.1",
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import cors from 'koa2-cors'
+import logger from 'koa-logger'
+
+import {config} from './config'
+import healthcheckRoutes from './routes/healthcheck'
+
+const app = new Koa()
+const PORT = config.port
+
+app.use(bodyParser())
+app.use(cors({origin:'*'}))
+app.use(logger())
+
+
+app.use(healthcheckRoutes.routes())
+
+const server = app.listen(PORT, async()=>{
+    console.log(`server listening on port ${PORT}`)
+}).on('error',err=>{
+    console.log(err)
+})
+export default server
